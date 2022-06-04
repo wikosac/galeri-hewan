5822df5cb72712d2e1fdaf83dd66dd587756ca57
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-06-05 00:05:20 +0700

    Mempelajari daur hidup Activity

--- app/src/main/java/org/d3if4401/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if4401/galerihewan/MainActivity.kt
@@ -1,11 +1,35 @@
 package org.d3if4401.galerihewan
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("MainActivity", "onCreate dijalankan")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart dijalankan")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume dijalankan")
+    }
+    override fun onPause() {
+        Log.i("MainActivity", "onPause dijalankan")
+        super.onPause()
+    }
+    override fun onStop() {
+        Log.i("MainActivity", "onStop dijalankan")
+        super.onStop()
+    }
+    override fun onDestroy() {
+        Log.i("MainActivity", "onDestroy dijalankan")
+        super.onDestroy()
     }
 }
