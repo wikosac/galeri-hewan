ae5f1a14a8ad9060348b0bcd8597a55b09a9da30
Dwiko Indrawansyah <wesibrani@gmail.com>
2022-06-25 16:09:22 +0700

    Membuat proses di background

--- app/src/main/java/org/d3if4401/galerihewan/ui/main/MainFragment.kt
+++ app/src/main/java/org/d3if4401/galerihewan/ui/main/MainFragment.kt
@@ -51,6 +51,7 @@ class MainFragment : Fragment() {
         viewModel.getStatus().observe(viewLifecycleOwner, {
             updateProgress(it)
         })
+        viewModel.scheduleUpdater(requireActivity().application)
     }
 
     private fun updateProgress(status: ApiStatus) {
--- app/src/main/java/org/d3if4401/galerihewan/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if4401/galerihewan/ui/main/MainViewModel.kt
@@ -1,16 +1,22 @@
 package org.d3if4401.galerihewan.ui.main
 
+import android.app.Application
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.d3if4401.galerihewan.R
 import org.d3if4401.galerihewan.model.Hewan
 import org.d3if4401.galerihewan.network.ApiStatus
 import org.d3if4401.galerihewan.network.HewanApi
+import org.d3if4401.galerihewan.network.UpdateWorker
+import java.util.concurrent.TimeUnit
 
 class MainViewModel : ViewModel() {
     private val data = MutableLiveData<List<Hewan>>()
@@ -36,4 +42,15 @@ class MainViewModel : ViewModel() {
     fun getData(): LiveData<List<Hewan>> = data
 
     fun getStatus(): LiveData<ApiStatus> = status
+
+    fun scheduleUpdater(app: Application) {
+        val request = OneTimeWorkRequestBuilder<UpdateWorker>()
+            .setInitialDelay(1, TimeUnit.MINUTES)
+            .build()
+        WorkManager.getInstance(app).enqueueUniqueWork(
+            "updater",
+            ExistingWorkPolicy.REPLACE,
+            request
+        )
+    }
 }
